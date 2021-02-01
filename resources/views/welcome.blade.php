437475310925ead24c852e32b8ca216a8ec030af
seba-gs <sebastian.gonlzaezsa@sansano.usm.cl>
2021-01-31 23:52:07 -0300

    comit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Animalitos V Region</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="">Inicio</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="">Catalogo</a>
+                </li>
+                <li class="nav-item active" href="">
+                    <a class="nav-link" href="">Chat</a>
+                </li>
+                <li class="nav-item active" href="">
+                    <a class="nav-link" href="">Noticias</a>
+                </li>
+                <li class="nav-item active" href="">
+                    <a class="nav-link" href="">Patrocinadores</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Busqueda" aria-label="Busqueda">
+                    <button class="btn btn-outline-success" type="submit">Buscar</button>
+                  </form>
+                <li class="nav-item active" href="">
+                    <a class="nav-link" href="">iniciar sesion</a>
+                </li>
+                <li class="nav-item active" href="">
+                    <a class="nav-link" href="">registrarse</a>
+                </li>
+            </ul>
+        </div>
+    </nav> 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>    
 </body>
 </html>

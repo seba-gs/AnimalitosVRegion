933872ad129f3e5ad41114bd3a3991ca74d55995
seba-gs <sebastian.gonlzaezsa@sansano.usm.cl>
2021-02-02 17:39:53 -0300

    commit

--- resources/views/general/index.blade.php
+++ resources/views/general/index.blade.php
@@ -1,4 +1,25 @@
 @extends('welcome')
 @section('contenido-principal')
-    
+    <div class="container">
+        <div class="row">
+            <div class="col logo">
+                <img src="images/logo.png">
+            </div>
+            <div class="col" style="align-content">
+                <h1>Animalitos V Region</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col rrss">
+                <p>
+                    Aqui iran las redes sociales que pondre despues
+                </p>
+            </div>
+            <div class="col parrafo">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non urna sed massa sodales tempus a vitae diam. Quisque nunc sem, efficitur pellentesque commodo sed, pellentesque in nisl. Quisque massa magna, volutpat sit amet erat ut, ullamcorper tristique mi. Integer id ex vitae odio condimentum viverra. Ut condimentum nisi enim, et aliquam leo placerat in. In eget semper tellus, sed egestas mi. In vestibulum dapibus enim eu mattis. Nullam nec hendrerit ipsum. Morbi aliquam, ex nec vestibulum bibendum, dolor tortor dictum turpis, id congue ligula elit id ante. Cras blandit, metus eget luctus volutpat, metus erat volutpat enim, sed dapibus sem mi at lorem. Nunc egestas risus ligula, eget consectetur ante molestie id. Suspendisse condimentum velit porttitor aliquet lobortis.
+                </p>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
     <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link rel="stylesheet" type="text/css" href="css/inicio.css">
+
+    <style>
+        h1 { color: #ca1a1a; }
+        p { color: #ca1a1a }
+      </style>
+    
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
